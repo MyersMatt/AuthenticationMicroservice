28988ce84e02dc70ec1974693d2f6aa21fc94df1
Matt Myers <myersmatt1987@gmail.com>
2022-10-13 15:14:43 -0700

    added basic models and template for connecting to database

---
+++ src/main/java/com/example/authenticationservice/models/User.java
@@ -0,0 +1,22 @@
+package com.example.authenticationservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USR_SEQ")
+    @SequenceGenerator(name="USR_SEQ", sequenceName = "USR_SEQ")
+    private long id;
+    private String username;
+    private String password;
+}
